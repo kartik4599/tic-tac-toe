c6f2dcb85a3050d38990d4feb0069859b196297d
Adit21 <kartikmendu@gmail.com>
2024-05-06 13:29:06 +0530

    Added Auto Play feture in game.

--- src/App.tsx
+++ src/App.tsx
@@ -5,31 +5,67 @@ import GameComponent from "./GameComponent";
 function App() {
   const [size, setSize] = useState(3);
   const [start, setStart] = useState(false);
+  // const [computer, setComputer] = useState(false);
 
   if (start) return <GameComponent size={size} setStart={setStart} />;
 
   return (
     <div>
       <h2>Tic-Tac-Toe</h2>
-      <div>
-        <label>Enter the Size</label>
-        <input
-          value={size}
-          type="number"
-          style={{
-            margin: "0 10px",
-            width: "35px",
-            padding: "10px",
-            paddingLeft: "25px",
-            borderRadius: "5px",
-            border: "none",
-          }}
-          min={2}
-          onChange={({ target: { value } }) => {
-            let currentSize = parseInt(value);
-            if (currentSize > 1) setSize(parseInt(value));
-          }}
-        />
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "start",
+          alignItems: "start",
+          margin: "20px",
+          gap: "10px",
+        }}
+      >
+        <div>
+          <label>Enter the Size</label>
+          <input
+            value={size}
+            type="number"
+            style={{
+              margin: "0 10px",
+              width: "35px",
+              padding: "10px",
+              paddingLeft: "25px",
+              borderRadius: "5px",
+              border: "none",
+            }}
+            min={2}
+            max={10}
+            onChange={({ target: { value } }) => {
+              let currentSize = parseInt(value);
+              if (currentSize > 1 && currentSize < 11) setSize(parseInt(value));
+            }}
+          />
+        </div>
+        {/* <div>
+          <label>Enter the Type</label>
+          <select
+            style={{
+              margin: "0 10px",
+              padding: "10px",
+              paddingLeft: "25px",
+              borderRadius: "5px",
+              border: "none",
+            }}
+            value={computer ? "2" : "1"}
+            onChange={({ target: { value } }) => {
+              if (value === "1") {
+                setComputer(false);
+              } else if (value === "2") {
+                setComputer(true);
+              }
+            }}
+          >
+            <option value="1">1 X 1</option>
+            <option value="2">1 X Computer</option>
+          </select>
+        </div> */}
         <button onClick={() => setStart(true)}>Start the Game</button>
       </div>
     </div>
--- src/GameComponent.tsx
+++ src/GameComponent.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useRef, useState } from "react";
 
 const Box = ({
   value,
@@ -57,74 +57,100 @@ const GameComponent = ({
   ]);
   const [winner, setWinner] = useState<null | string>(null);
   const [count, setCount] = useState(0);
-  const checkResult = (
-    matrix: {
-      x: number;
-      y: number;
-      value: null | boolean;
-    }[]
-  ) => {
-    const obj: Record<string, any> = {
-      x: {},
-      y: {},
-      z: 0,
-      w: 0,
-    };
-    matrix.forEach(({ x, y }) => {
-      if (obj.x[x] === undefined) obj.x[x] = 0;
-      if (obj.y[y] === undefined) obj.y[y] = 0;
-      obj.x[x]++;
-      obj.y[y]++;
-      if (x === y) obj.z++;
-      if (x + y === size - 1) obj.w++;
-    });
-    let result = false;
-    result = Object.values(obj.x).includes(size);
-    result = result ? result : Object.values(obj.y).includes(size);
-    result = result ? result : obj.w === size;
-    result = result ? result : obj.z === size;
-    return result;
-  };
+  const [time, setTime] = useState(30);
+  let timeRef = useRef(0);
 
-  const filterCheck = (checkValue: boolean) => {
-    const data = matrix.filter(({ value }) => value === checkValue);
-    if (data.length === 0) return;
-    if (checkResult(data)) return setWinner(checkValue ? "X" : "O");
-    if (data.length > size) {
-      let { x, y } = data.sort(({ count: a }, { count: b }) => a - b)[0];
-      setMatrix((pre) =>
-        pre.map((data) =>
-          data.x === x && data.y === y
-            ? { ...data, count: 0, value: null }
-            : data
-        )
-      );
-    }
-  };
+  const checkResult = useCallback(
+    (
+      matrix: {
+        x: number;
+        y: number;
+        value: null | boolean;
+      }[]
+    ) => {
+      const obj: Record<string, any> = {
+        x: {},
+        y: {},
+        z: 0,
+        w: 0,
+      };
+      matrix.forEach(({ x, y }) => {
+        if (obj.x[x] === undefined) obj.x[x] = 0;
+        if (obj.y[y] === undefined) obj.y[y] = 0;
+        obj.x[x]++;
+        obj.y[y]++;
+        if (x === y) obj.z++;
+        if (x + y === size - 1) obj.w++;
+      });
+      let result = false;
+      result = Object.values(obj.x).includes(size);
+      result = result ? result : Object.values(obj.y).includes(size);
+      result = result ? result : obj.w === size;
+      result = result ? result : obj.z === size;
+      return result;
+    },
+    []
+  );
 
-  const checkGame = () => {
+  const filterCheck = useCallback(
+    (checkValue: boolean) => {
+      const data = matrix.filter(({ value }) => value === checkValue);
+      if (data.length === 0) return;
+      if (checkResult(data)) return setWinner(checkValue ? "X" : "O");
+      if (data.length > size) {
+        let { x, y } = data.sort(({ count: a }, { count: b }) => a - b)[0];
+        setMatrix((pre) =>
+          pre.map((data) =>
+            data.x === x && data.y === y
+              ? { ...data, count: 0, value: null }
+              : data
+          )
+        );
+      }
+    },
+    [matrix]
+  );
+
+  const checkGame = useCallback(() => {
     filterCheck(true);
     filterCheck(false);
+  }, [filterCheck]);
+
+  const autoPlay = () => {
+    const data = matrix.filter(({ value }) => value === null);
+    console.log(data);
+    if (data.length === 0) return;
+    const index = Math.floor(Math.random() * (data.length - 1));
+    clickEvent(data[index].x, data[index].y);
   };
 
-  const clickEvent = (X: number, Y: number) => {
-    if (winner) return;
-    setMatrix((pre) => {
-      const index = pre.findIndex(({ x, y }) => x === X && y === Y);
-      if (pre[index].value !== null) return pre;
-      pre[index] = { ...pre[index], value: currentPlay, count: count + 1 };
-      setCurrentPlay((p) => !p);
-      return pre;
-    });
-    setCount(count + 1);
+  const clickEvent = useCallback(
+    (X: number, Y: number) => {
+      if (winner) return;
+      setMatrix((pre) => {
+        const index = pre.findIndex(({ x, y }) => x === X && y === Y);
+        if (pre[index].value !== null) return pre;
+        pre[index] = { ...pre[index], value: currentPlay, count: count + 1 };
+        setCurrentPlay((p) => !p);
+        setCount(count + 1);
+        setTime(30);
+        return pre;
+      });
+    },
+    [setMatrix, setCurrentPlay, winner, count, currentPlay, setCount, matrix]
+  );
+
+  const resetTime = () => {
+    setTime(30);
+    autoPlay();
   };
 
-  const restartGame = () => {
+  const restartGame = useCallback(() => {
     setMatrix((pre) => pre.map((data) => ({ ...data, value: null })));
     setWinner(null);
     setCurrentPlay(true);
     setStart(false);
-  };
+  }, [setMatrix, setWinner, setCurrentPlay, setStart]);
 
   useEffect(checkGame, [currentPlay]);
 
@@ -144,34 +170,96 @@ const GameComponent = ({
     }
     setMatrix(boxes);
     setGridTemplateColumns(Array(size).fill("auto"));
-  }, [size]);
+    timeRef.current = setInterval(() => setTime((pre) => pre - 1), 1000);
+  }, [size, setTime]);
+
+  useEffect(() => {
+    if (time === 0) {
+      resetTime();
+    }
+  }, [time]);
+
+  useEffect(() => {
+    if (winner) {
+      clearInterval(timeRef.current);
+    }
+  }, [winner]);
 
   return (
     <>
       <div
         style={{
-          display: "grid",
-          gridTemplateColumns: gridTemplateColumns.join(" "),
+          display: "flex",
+          width: "70vw",
+          justifyContent: "space-between",
         }}
       >
-        {matrix.map(({ value, x, y, count: ct }, index) => (
-          <Box
-            key={index}
-            value={value}
-            count={ct}
-            currentCurrentCount={count}
-            clickEvent={clickEvent.bind(null, x, y)}
+        <div></div>
+        {/* <div>
+          Type :{" "}
+          <input
+            type="text"
+            style={{
+              width: "fir-content",
+              height: "25px",
+              padding: "0px 8px",
+              border: "none",
+            }}
+            readOnly
+            value={computer ? "1 X Computer" : "1 X 1"}
           />
-        ))}
-      </div>
-      {winner && (
-        <div style={{ display: "flex", flexDirection: "column" }}>
-          <span>Winner is {winner}</span>
-          <button onClick={restartGame} style={{ marginTop: "10px" }}>
-            Restart
-          </button>
+        </div> */}
+        <div>
+          Remaining Time :{" "}
+          <input
+            type="text"
+            style={{
+              width: "40px",
+              height: "25px",
+              padding: "0px 8px",
+              border: "none",
+            }}
+            readOnly
+            value={`00:${time.toString().padStart(2, "0")}`}
+          />
         </div>
-      )}
+      </div>
+      <div
+        style={{
+          minHeight: "90vh",
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          alignItems: "center",
+          width: "100%",
+        }}
+      >
+        <div
+          style={{
+            display: "grid",
+            gridTemplateColumns: gridTemplateColumns.join(" "),
+            width: "fit-content",
+          }}
+        >
+          {matrix.map(({ value, x, y, count: ct }, index) => (
+            <Box
+              key={index}
+              value={value}
+              count={ct}
+              currentCurrentCount={count}
+              clickEvent={clickEvent.bind(null, x, y)}
+            />
+          ))}
+        </div>
+        {winner && (
+          <div style={{ display: "flex", flexDirection: "column" }}>
+            <span>Winner is {winner}</span>
+            <button onClick={restartGame} style={{ marginTop: "10px" }}>
+              Restart
+            </button>
+          </div>
+        )}
+      </div>
     </>
   );
 };
